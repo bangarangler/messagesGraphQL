05a632281efdf35301fb1141af18b5d88ba57c74
Jonathan Dain Palacio <bangarangler@gmail.com>
2020-02-08 15:05:04 -0500

    if a entity don't have messages, but message...
      identifiers, you can write a custom resolver for the messages
      of a user. this resolver retrieves all messages from the user
      from the list of sample messages.

--- index.js
+++ index.js
@@ -9,11 +9,26 @@ app.use(cors())
 let users = {
   1: {
     id: "1",
-    username: 'Jon Dain'
+    username: 'Jon Dain',
+    messageIds: [1]
   },
   2: {
     id: "2",
-    username: 'Kevin Bacon'
+    username: 'Kevin Bacon',
+    messageIds: [2]
+  }
+}
+
+let messages = {
+  1: {
+    id: "1",
+    text: "Hello World",
+    userId: '1'
+  },
+  2: {
+    id: "2",
+    text: "By World",
+    userId: "2"
   }
 }
 
@@ -23,11 +38,20 @@ const schema = gql`
     users: [User!]
     user(id: ID!): User
     me: User
+    messages: [Message!]!
+    message(id: ID!): Message!
   }
 
   type User {
     id: ID!
     username: String!
+    messages: [Message!]
+  }
+
+  type Message {
+    id: ID!
+    text: String!
+    user: User!
   }
 `
 const resolvers = {
@@ -40,8 +64,26 @@ const resolvers = {
     },
     me: (parent, args, { me }, info) => {
       return me;
+    },
+    // messages: () => {
+    //   return Object.values(messages)
+    // },
+    message: (parent, { id }) => {
+      return messages[id]
     }
   },
+  User: {
+    messages: user => {
+      return Object.values(messages).filter(
+        message => message.userId === user.id
+      )
+    }
+  },
+  Message: {
+    user: (message) => {
+      return users[message.userId]
+    }
+  }
 }
 
   const server = new ApolloServer({
