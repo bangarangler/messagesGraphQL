0a56ff9547f7467d2d59ceb40b21aeb06f479ca4
Jonathan Dain Palacio <bangarangler@gmail.com>
2020-02-08 14:37:25 -0500

    hooked up apollo-server-express and tested...
    3 Query's with graphql playground and some dummy data.

---
+++ index.js
@@ -0,0 +1,57 @@
+const cors = require('cors')
+const express = require('express');
+const { ApolloServer, gql } = require('apollo-server-express');
+
+const app = express()
+
+app.use(cors())
+
+let users = {
+  1: {
+    id: "1",
+    username: 'Jon Dain'
+  },
+  2: {
+    id: "2",
+    username: 'Kevin Bacon'
+  }
+}
+
+const me = users[1]
+
+const schema = gql`
+  type Query {
+    users: [User!]
+    user(id: ID!): User
+    me: User
+  }
+
+  type User {
+    id: ID!
+    username: String!
+  }
+`
+const resolvers = {
+  Query: {
+    users: () => {
+      return Object.values(users)
+    },
+    user: (parent, { id }) => {
+      return users[id]
+    },
+    me: () => {
+      return me;
+    }
+  }
+}
+
+  const server = new ApolloServer({
+    typeDefs: schema,
+    resolvers,
+  })
+
+server.applyMiddleware({ app, path: '/graphql' })
+
+app.listen({port: 8000}, () => {
+  console.log('Apollo Server on http://localhost:8000/graphql')
+})
